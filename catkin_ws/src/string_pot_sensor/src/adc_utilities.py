d64828a5f590c1ef0dd755fccd824601d72eb5d7
becomesaflame <spark.scul@gmail.com>
2016-06-12 22:02:19 +0000

    Created string_pot_sensor ROS package. Issue #3

---
+++ catkin_ws/src/string_pot_sensor/src/adc_utilities.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+"""
+"""
+
+from glob import glob
+import logging
+import os
+from utilities import clamp, getPercentageIntoRange, readInt
+import logging
+import math
+
+log = logging.getLogger(__name__)
+
+"""
+ Get an ADC reading representing a string pot value
+ Map that value to an angle based on allowable range
+ of pot value and angle for that joint
+"""
+def strPot_to_angle(strPot, potRange, pistonLengthRange, offsets):
+  strPot = clamp(potRange, strPot) 
+  potSpan = potRange[1] - potRange[0]
+  pistonLengthSpan = pistonLengthRange[1] - pistonLengthRange[0]
+  pistonLength = (((strPot - potRange[0]) * potSpan)/ pistonLengthSpan ) + pistonLengthRange[0]
+  # law of cosines
+  return math.acos((offsets[0]**2 + offsets[1]**2 - pistonLength)/(2 * offsets[0] * offsets[1])) 
+
+def readAin(pinName, jointRange):
+  raw = readInt(pinName)
+  raw = clamp(jointRange, raw)
+  return getPercentageIntoRange(jointRange, float(raw))
+
+def getAinPinName(pin):
+  hits = glob("/sys/bus/iio/devices/iio:device?/in_voltage%s_raw" % pin)
+  if hits:
+    return hits[0]
+  else:
+    log.fatal("Could not find ADC pin %s, double check that the firmware loaded..." % pin)
+    from pwm_utilities import stopEverythingAndQuit
+    stopEverythingAndQuit()
+
+AIN3 = getAinPinName(3)
+AIN4 = getAinPinName(4)
+AIN5 = getAinPinName(5)
+KNEE_RANGE = (0, 4096)  # these values were pulled out of my ass
+ELBOW_RANGE = (1420, 3570)  # these values came from a run of calibrate.py
+SHOULDER_RANGE = (845, 2736)  # these values were measured manually
+# 2465
+if __name__ == "__main__":
+  pass
---
+++ catkin_ws/src/string_pot_sensor/src/sensor.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2008, Willow Garage, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of Willow Garage, Inc. nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Revision $Id$
+
+## Simple sensor demo that published std_msgs/Strings messages
+## to the 'chatter' topic
+
+from math import pi
+
+import rospy
+import sensor_msgs
+
+from adc_utilities import * 
+from utilities import *
+
+from geometry_msgs.msg import Quaternion
+
+def sensor():
+    pub = rospy.Publisher('JointState', sensor_msgs.JointState, queue_size=10)
+    rospy.init_node('StringPotSensor', anonymous=True)
+    rate = rospy.Rate(100) # 100hz
+    while not rospy.is_shutdown():
+        rawStringPot = readAin(AIN4, KNEE_RANGE)
+        rospy.loginfo(rawStringPot)
+
+	radianAngle = strPot_to_angle(rawStringPot, KNEE_POT_RANGE, KNEE_ANGLE_RANGE)
+	rospy.loginfo(radianAngle)
+
+        pub.publish(radianAngle)
+        rate.sleep()
+
+KNEE_POT_RANGE = (0, 4096)
+KNEE_ANGLE_RANGE = (0,180)
+
+if __name__ == '__main__':
+    try:
+        sensor()
+    except rospy.ROSInterruptException:
+        pass
